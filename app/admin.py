7081565edf2b50d3bb92e3dfb4c5e58d93af03bb
jakha921 <jakha921@gmail.com>
2024-07-08 01:30:57 +0500

    custom admin

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
+
 from .models import Category, Post
 
-admin.site.register(Category)
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'created_at', 'category')
+    list_filter = ('status', 'created_at')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'created_at'
+    search_fields = ('title', 'content')
+    ordering = ('status', 'created_at')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+    search_fields = ('name',)
+    ordering = ('name',)
